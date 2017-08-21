2113a425e4ea1fd287d686622584dc9e9c720bc2
bluecreator <bluecreator@DESKTOP-C9QC08E>
2017-08-21 09:43:54 +0900

    템플릿 콜백

--- toby/src/main/java/springbook/learningtest/template/CalcSumTest.java
+++ toby/src/main/java/springbook/learningtest/template/CalcSumTest.java
@@ -1,12 +1,39 @@
 package springbook.learningtest.template;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcSumTest {
 	
+	Calculator calculator;
+	String numFilepath;
+	
+	@Before
+	public void setUp(){
+		calculator = new Calculator();
+		numFilepath = getClass().getResource("numbers.txt").getPath();
+	}
+	
 	@Test
-	public void sumOfNumbers(){
-		
+	public void sumOfNumbers() throws IOException{
+		int sum = calculator.calcSum(numFilepath);
+		assertThat(sum, is(10));
+	}
+	
+	@Test
+	public void multiplyOfNumbers() throws IOException{
+		int sum = calculator.calcMultiply(numFilepath);
+		assertThat(sum, is(24));
+	}
+	
+	@Test
+	public void concatenateStrings() throws IOException{
+		assertThat(calculator.concatenate(this.numFilepath), is("1234"));
 	}
 
 }
