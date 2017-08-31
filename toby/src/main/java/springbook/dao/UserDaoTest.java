b0c1c5cc846b67d42c1259cb96e6bdac20c27936
bluecreator <bluecreator@DESKTOP-C2SO88H>
2017-08-31 23:21:46 +0900

    test

--- toby/src/main/java/springbook/dao/UserDaoTest.java
+++ toby/src/main/java/springbook/dao/UserDaoTest.java
@@ -3,14 +3,20 @@ package springbook.dao;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.sql.SQLException;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -25,6 +31,9 @@ public class UserDaoTest {
 	ApplicationContext context;
 	
 	private UserDaoJdbc dao; 
+	
+	@Autowired
+	DataSource dataSource;
 	
 	private User user1;
 	private User user2;
@@ -114,4 +123,29 @@ public class UserDaoTest {
 		assertThat(user1.getName(), is(user2.getName()));
 		assertThat(user1.getPassword(), is(user2.getPassword()));
 	}
+	
+	
+	//@Test(expected=DataAccessException.class)
+	@Test(expected=DuplicateKeyException.class)
+	public void duplicateKey(){
+		dao.deleteAll();
+		dao.add(user1);
+		dao.add(user1);
+	}
+	
+	@Test
+	public void sqlExceptionTranslate(){
+		dao.deleteAll();
+		
+		try{
+			dao.add(user1);
+			dao.add(user1);
+		}catch(DuplicateKeyException ex){
+			SQLException sqlEx = (SQLException) ex.getRootCause();
+			SQLExceptionTranslator set = new SQLErrorCodeSQLExceptionTranslator(this.dataSource);
+			assertThat(set.translate(null, null, sqlEx), is(DuplicateKeyException.class));
+		}
+		
+	}
+	
 }
