e9193df2253d00f8a93ee7436edc41fa03799804
bluecreator <bluecreator@DESKTOP-C9QC08E>
2018-01-11 20:31:39 +0900

    test

--- toby/src/main/java/springbook/service/UserService.java
+++ toby/src/main/java/springbook/service/UserService.java
@@ -1,10 +1,17 @@
 package springbook.service;
 
+import java.util.List;
+
 import springbook.domain.User;
 
 public interface UserService {
 	
 	public void add(User user);
+	User get(String id);
+	List<User> getAll();
+	void deleteAll();
+	void update(User user);
+	
 	public void upgradeLevels();
 
 }
