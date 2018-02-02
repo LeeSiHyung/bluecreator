c7a9788a327be5558db41ea1fe215dab37392913
bluecreator <bluecreator@DESKTOP-C9QC08E>
2018-02-02 09:36:12 +0900

    test

---
+++ toby/src/main/java/springbook/service/sql/JaxbXmlSqlReader.java
@@ -0,0 +1,40 @@
+package springbook.service.sql;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import springbook.jaxb.SqlType;
+import springbook.jaxb.Sqlmap;
+
+public class JaxbXmlSqlReader implements SqlReader{
+	
+	private String sqlmapFile;
+
+	@Override
+	public void read(SqlRegistry sqlRegistry) {
+		
+		String contextPath = Sqlmap.class.getPackage().getName();
+		try {
+			
+			JAXBContext context = JAXBContext.newInstance(contextPath);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			InputStream is = getClass().getResourceAsStream(sqlmapFile);
+			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+			for(SqlType sql : sqlmap.getSql()){
+				sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+			}
+			
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
+	}
+
+}
