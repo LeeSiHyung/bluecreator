dfe93e8cffdd06a6929da828f9324e771022d4a4
bluecreator <bluecreator@DESKTOP-C9QC08E>
2018-02-15 15:52:13 +0900

    test

---
+++ toby/src/main/java/springbook/exception/SqlUpdateFailureException.java
@@ -0,0 +1,13 @@
+package springbook.exception;
+
+public class SqlUpdateFailureException extends RuntimeException{
+	
+	
+	private static final long serialVersionUID = -6416617882543518275L;
+	
+	
+	public SqlUpdateFailureException() {
+	}
+	
+
+}
