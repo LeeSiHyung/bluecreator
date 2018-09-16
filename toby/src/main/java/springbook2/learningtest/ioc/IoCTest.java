ca7d93c06a01af0c7971ed4dfb1d7424fd2638dc
bluecreator <bluecreator@DESKTOP-C9QC08E>
2018-09-16 09:48:44 +0900

    # WARNING: head commit changed in the meantime
    test

--- toby/src/main/java/springbook2/learningtest/ioc/IoCTest.java
+++ toby/src/main/java/springbook2/learningtest/ioc/IoCTest.java
@@ -11,11 +11,14 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -117,6 +120,29 @@ public class IoCTest {
 		hello.print();
 		
 		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+		
+	}
+	
+	
+	@Test
+	public void 부모_자식_컨텍스트_테스트() {
+		
+		String basePath = StringUtils.cleanPath(ClassUtils.classPackageAsResourcePath(getClass()) + "/");
+		
+		ApplicationContext parent = new GenericXmlApplicationContext(basePath + "parentContext.xml");
+		GenericApplicationContext child = new GenericApplicationContext(parent);
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+		reader.loadBeanDefinitions(basePath + "childContext.xml");
+		child.refresh();
+		
+		Printer printer = child.getBean("printer", Printer.class);
+		assertThat(printer, is(notNullValue()));
+		
+		Hello hello = child.getBean("hello", Hello.class);
+		assertThat(hello, is(notNullValue()));
+		
+		hello.print();
+		assertThat(printer.toString(), is("Hello Child"));
 		
 	}
 	
