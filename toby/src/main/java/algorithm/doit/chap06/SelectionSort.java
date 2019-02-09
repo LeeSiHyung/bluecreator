03db999be08305a142fca091cd40e55fe9d50e3c
dltlg <dltlgud1324@gmail.com>
2019-02-09 12:26:41 +0900

    정렬 커밋

--- toby/src/main/java/algorithm/doit/chap06/SelectionSort.java
+++ toby/src/main/java/algorithm/doit/chap06/SelectionSort.java
@@ -11,9 +11,9 @@ public class SelectionSort {
 	}
 	
 	static void selectionSort(int[] a, int n) {
-		for(int i=0; i < n - 1; i++) {
+		for(int i=0; i < n-1; i++) {
 			int min = i;
-			for(int j= i + 1; j < n; j++) {
+			for(int j=i+1; j < n; j++) {
 				if(a[min] > a[j]) {
 					min = j;
 				}
